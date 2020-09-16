644c0f9f5ad4e9482909a4595f1f38b5cd3274c6
Meet191100 <meet19jeet21@gmail.com>
2020-09-16 18:43:28 +0530

    project push

---
+++ app/src/main/java/com/developer/arsltech/covid_19tracker/SlideViewPagerAdapter.java
@@ -0,0 +1,131 @@
+package com.developer.arsltech.covid_19tracker;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.developer.arsltech.Covid19.R;
+
+public class SlideViewPagerAdapter extends PagerAdapter {
+
+    Context ctx;
+
+    public SlideViewPagerAdapter(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
+        LayoutInflater layoutInflater= (LayoutInflater) ctx.getSystemService(ctx.LAYOUT_INFLATER_SERVICE);
+        View view=layoutInflater.inflate(R.layout.slide_screen,container,false);
+
+
+        ImageView logo=view.findViewById(R.id.logo);
+
+        ImageView ind1=view.findViewById(R.id.ind1);
+        ImageView ind2=view.findViewById(R.id.ind2);
+        ImageView ind3=view.findViewById(R.id.ind3);
+
+
+        TextView title=view.findViewById(R.id.title);
+        TextView desc=view.findViewById(R.id.desc);
+
+        ImageView next=view.findViewById(R.id.next);
+        ImageView back=view.findViewById(R.id.back);
+
+        Button btnGetStarted=view.findViewById(R.id.btnGetStarted);
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ctx,MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+                ctx.startActivity(intent);
+            }
+        });
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SlideActivity.viewPager.setCurrentItem(position+1);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SlideActivity.viewPager.setCurrentItem(position-1);
+            }
+        });
+
+        switch (position)
+        {
+            case 0:
+                logo.setImageResource(R.drawable.logo1);
+                ind1.setImageResource(R.drawable.seleted);
+                ind2.setImageResource(R.drawable.unselected);
+                ind3.setImageResource(R.drawable.unselected);
+
+                title.setText("Keep Wear Mask");
+                desc.setText("If you need to be around other people, wear a mask. The mask helps prevent a person who is sick from spreading the virus to others. It helps keep respiratory droplets contained and from reaching other people.");
+                back.setVisibility(View.GONE);
+                next.setVisibility(View.VISIBLE);
+                break;
+            case 1:
+                logo.setImageResource(R.drawable.logo2);
+                ind1.setImageResource(R.drawable.unselected);
+                ind2.setImageResource(R.drawable.seleted);
+                ind3.setImageResource(R.drawable.unselected);
+
+                title.setText("Keep Wash Your Hands");
+                desc.setText("Be a germ buster,Be aware, wash with care, Break the germ cycle, Bury the germ, Clean handed people are clean. So be clean, wash your hands.");
+                back.setVisibility(View.VISIBLE);
+                next.setVisibility(View.VISIBLE);
+                break;
+            case 2:
+                logo.setImageResource(R.drawable.logo3);
+                ind1.setImageResource(R.drawable.unselected);
+                ind2.setImageResource(R.drawable.unselected);
+                ind3.setImageResource(R.drawable.seleted);
+
+                title.setText("Maintain Social Distance");
+                desc.setText("Working from home instead of at the office, Closing schools or switching to online classes, Visiting loved ones by electronic devices instead of in person, Cancelling or postponing conferences and large meetings.");
+                back.setVisibility(View.VISIBLE);
+                next.setVisibility(View.GONE);
+                break;
+
+
+
+
+
+
+        }
+
+
+        container.addView(view);
+        return view;
+
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
